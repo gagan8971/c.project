9f932f8887d8eee98abda6e78454c73b67f8fa1d
gagan8971 <vara7adigagan@gmail.com>
2025-11-12 13:58:31 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
-main() {
+#include <stdio.h>
 
-biggest3();
-factorial();
+/* Function declarations */
+void biggest3();
+void factorial();
+
+int main() {
+    biggest3();
+    factorial();
+    return 0;
 }
